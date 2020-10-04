03b17dfa3571e4610051e26191cf604c627a32be
justynalezuch <justynalezuch@gmail.com>
2020-10-04 17:55:20 +0200

    Registration and login - PDO queries

--- registration.php
+++ registration.php
@@ -18,7 +18,7 @@
             $all_right = false;
             $_SESSION['e_username'] = "Nazwa użytkownika może składać się tylko z liter i cyfr (bez polskich znaków)";
         }
-        
+
         if (empty($email)) {
             $all_right = false;
             $_SESSION['e_email'] = "Podaj poprawny adres email.";
@@ -57,49 +57,50 @@
         $_SESSION['fr_password'] = $password;
         $_SESSION['fr_confirm_password'] = $confirm_password;
 
-//        require_once 'connect.php';
-        mysqli_report(MYSQLI_REPORT_STRICT);
-//
-//        try {
-//            $connect = new mysqli($host, $db_user, $db_password, $db_name);
-//            if ($connect->connect_errno!=0) {
-//                throw new Exception(mysqli_connect_errno());
-//            }
-//            else {
-//                // Is the email already reserved?
-//                $result = $connect->query("SELECT id FROM users WHERE email='$email'");
-//                if(!$result) throw new Exception($connect->error);
-//
-//                $how_many_emails = $result->num_rows;
-//                if($how_many_emails>0) {
-//                    $all_right = false;
-//                    $_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail.";
-//                }
-//
-//                //Is the nickname already reserved?
-//                $result = $connect->query("SELECT id FROM users WHERE username='$username'");
-//                if(!$result) throw new Exception($connect->error);
-//                $how_many_usernames = $result->num_rows;
-//                if($how_many_usernames>0) {
-//                    $all_right = false;
-//                    $_SESSION['e_username'] = "Istnieje już użytkownik z taką nazwą.";
-//                }
-//
-//                $connect->close();
-//            }
-//        } catch (Exception $e) {
-//            echo '<div class="error">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</div>';
-//            echo 'Informacja developerska: '.$e;
-//        }
+        $config = require_once 'config.php';
+        try {
 
+            $db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password'], [
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+
+            $query = $db->prepare('SELECT id FROM users WHERE email=:email');
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->execute();
+
+            if($query->fetch()) {
+                $all_right = false;
+                $_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail.";
+            }
+
+            $query = $db->prepare('SELECT id FROM users WHERE username=:username');
+            $query->bindValue(':username', $username, PDO::PARAM_STR);
+            $query->execute();
+
+            if($query->fetch()) {
+                $all_right = false;
+                $_SESSION['e_username']="Istnieje już użytkownik z taką nazwą.";
+            }
+
+        } catch (PDOException $error) {
+
+            echo $error->getMessage();
+            exit('Database error.');
+
+        }
 
         if($all_right == true) {
-            // Add to database
-            if($connect->query("INSERT INTO users VALUES (null, '$username', '$password_hash', '$email')")){
+
+            $query = $db->prepare('INSERT INTO users VALUES (NULL, :username, :password_hash, :email)');
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->bindValue(':username', $username, PDO::PARAM_STR);
+            $query->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+            $query->execute();
+
+            if($query->execute()) {
                 $_SESSION['successful_registration'] = true;
                 header('Location: welcome.php');
-            } else {
-                throw new Exception($connect->error);
             }
         }
     }
